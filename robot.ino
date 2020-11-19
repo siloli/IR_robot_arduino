c083c9e00f11c0524a96dce456e05af12d3b4ea8
siloli <48384046+siloli@users.noreply.github.com>
2020-11-19 16:49:11 +0100

    Update robot.ino
    incompatibilité des librairies AFMotor et IRremote. Les moteurs ne tournent pas
    Fin de l'exercice, pour le moment

--- robot.ino
+++ robot.ino
@@ -1,81 +1,95 @@
-#include <AFMotor.h>
 #include <IRremote.h>
-#define power 0xFFA25D
-#define right
-#define left
-#define backward
-#define forward
-#define temps 500
-#define moteur_droite
-#define moteur_gauche
-#define speed 200
-int run=1
-AF_DCMotor droite(moteur_droite,MOTOR12_64KHZ);
-AF_DCMotor gauche(moteur_gauche,MOTOR12_64KHZ);
+
+#include <AFMotor.h>
+
+#define power 0xE318261B
+#define right 0x20FE4DBB
+#define left 0x52A3D41F
+#define backward 0xA3C8EDDB
+#define forward 0x511DBB
+#define set_up 0xEE886D7F
+#define temps 5000
+#define moteur_droite 1
+#define moteur_gauche 2
+#define motor_speed 255
+#define IR 13
+
+int is_running = 1;
+long unsigned dump_IR = 0;
+AF_DCMotor droite(moteur_droite);
+AF_DCMotor gauche(moteur_gauche);
 IRrecv irrecv(IR);
 decode_results results;
 
-
-void setup(){
-    irrecv.enableIRIn();
-    droite.setSpeed(speed);
-    gauche.setSpeed(speed);
+void setup() {
+  irrecv.enableIRIn();
+  droite.setSpeed(motor_speed);
+  gauche.setSpeed(motor_speed);
+  Serial.begin(9600);
+    Serial.println("en avant mon capitaine");
+  droite.run(FORWARD);
+  gauche.run(FORWARD);
+  delay(temps);
 }
 
+void loop() {
+  if (irrecv.decode(&results)) {
+    Serial.println(results.value, HEX);
+    switch (results.value) {
+      case power:
+        Serial.println("Power changement");
+        is_running = -is_running;
+        stop();
+        break;
+      case right:
+        if (is_running == 1)right_dir();
+        break;
+      case left:
+        if (is_running == 1)left_dir();
+        break;
+      case backward:
+        if (is_running == 1)backward_dir();
+        break;
+      case forward:
+        if (is_running == 1)forward_dir();
+        break;
+      default:
+        break;
+    }
+    irrecv.resume();
+  }
+}
 
-void loop(){
-    if(irrecv.decode(&results)){
-        switch(results.value){
-            case power:
-                run=-run;
-                break;
-            case right:
-                if (run==1)right_dir();
-                break;
-            case left:
-                if (run==1)left_dir();
-                break;
-            case backward:
-                if (run==1)backward_dir();
-                break;
-            case forward:
-                if (run==1)forward_dir();
-                break;
-        }
-        if(results.value!=0xFFFFFF)stop();
-        irrecv.resume();
-    }
+void stop() {
+  Serial.println("STOOOP"); 
+  Serial.print(is_running);
+  droite.run(RELEASE);
+  gauche.run(RELEASE);
 }
-void stop(){
-    droite.run(RELEASE);
-    gauche.run(RELEASE);
-    delay(temps);
+
+
+void right_dir() {
+  Serial.println("à droite, touuute");
+  droite.run(BACKWARD);
+  gauche.run(FORWARD);
+  delay(temps);
+
 }
-void right_dir(){
-    if(run==1){
-        droite.run(BACKWARD);
-        gauche.run(FORWARD);
-        delay(temps);
-    }
+void left_dir() {
+  Serial.println("à babord moussaillon");
+  droite.run(FORWARD);
+  gauche.run(BACKWARD);
+  delay(temps);
 }
-void left_dir(){
-    if(run==1){
-        droite.run(FORWARD);
-        gauche.run(BACKWARD);
-        delay(temps);
-    }
+void forward_dir() {
+  Serial.println("en avant mon capitaine");
+  droite.run(FORWARD);
+  gauche.run(FORWARD);
+  delay(temps);
 }
-void forward_dir(){
-    if(run==1){
-        droite.run(FORWARD);
-        gauche.run(FORWARD);
-        delay(temps);
-    }
-}
-void backward_dir(){
-    if(run==1){
-        droite.run(BACKWARD);
-        gauche.run(BACKWARD);
-        delay(temps);
-    }
+void backward_dir() {
+  Serial.println("maaarchee arrieeeeeeeere");
+  droite.run(BACKWARD);
+  gauche.run(BACKWARD);
+  delay(temps);
 }
